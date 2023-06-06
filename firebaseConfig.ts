6897084e8ab4caad743adb5d5866584a97666d0d
schienieder <schieniezel@gmail.com>
2023-06-06 13:37:19 +0800

    fix: firebase notifs not working

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyASJYdItsdTfI_RFQAtfIr24Y6_B8ulB-g",
-  authDomain: "fir-project-5de7a.firebaseapp.com",
-  projectId: "fir-project-5de7a",
-  storageBucket: "fir-project-5de7a.appspot.com",
-  messagingSenderId: "212833881842",
-  appId: "1:212833881842:web:60cd098f2ddddc26948a6b"
+  apiKey: "AIzaSyAeN5Sg_MG15vsTIYuW85L_gmS3SaX0NiQ",
+  authDomain: "dimts-project.firebaseapp.com",
+  projectId: "dimts-project",
+  storageBucket: "dimts-project.appspot.com",
+  messagingSenderId: "650219791304",
+  appId: "1:650219791304:web:baac99eb063e379a9616fd"
 };
 
 // Initialize Firebase
